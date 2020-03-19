99b108c921a15f72995ae9f36f0442b95c9cc500
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-20 00:06:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
+import UserProfile from "./user-profile/user-profile.component";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <UserProfile name="wchamra" email="wchamamra@yohobed.com" />
     </div>
   );
 }
